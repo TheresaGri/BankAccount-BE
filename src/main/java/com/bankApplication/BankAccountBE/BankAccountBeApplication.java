d94bf43863e30dc7828069a1f072058e960ee260
Theresa Grimus <T.Grimus@gmx.net>
2023-12-20 19:01:54 +0100

    Initial commit

---
+++ src/main/java/com/bankApplication/BankAccountBE/BankAccountBeApplication.java
@@ -0,0 +1,13 @@
+package com.bankApplication.BankAccountBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankApplication/BankAccountBE/BankAccountBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankApplication.BankAccountBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
